6a6b809933e98265f9733b84d4f3884a4ba22d42
dapr1nc3 <ddaprincem84@gmail.com>
2021-11-04 00:03:45 -0400

    added task-type, select dropdown,html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ buttonE1.addEventListener("click", function() {
     alert("button clicked");
   });
 
-  var buttonEl = document.querySelector("#save-task");
+var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
@@ -16,9 +16,5 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler); {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }; 
+  buttonEl.addEventListener("click", createTaskHandler); 
+    
