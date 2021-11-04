8298365b59ce1e71365369d2266182c3dfc99246
dapr1nc3 <ddaprincem84@gmail.com>
2021-11-04 00:32:56 -0400

    add handle form sub

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
 
-buttonE1.addEventListener("click", function() {
-    alert("button clicked");
-  });
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
-
-  buttonEl.addEventListener("click", createTaskHandler); 
-    
+  formEl.addEventListener("submit", createTaskHandler);
